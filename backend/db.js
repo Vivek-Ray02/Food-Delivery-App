c2fb4aea46ec36b329b8a5213bdeeb6cc39b907d
Vivek-Ray02 <gea1900319@gmail.com>
2024-04-12 09:52:18 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://Vivekray:Ironfist12@merncluster.tijdmu7.mongodb.net/FoodMERN?retryWrites=true&w=majority';
+
+const Schema = mongoose.Schema;
+const mySchema = new Schema({
+  
+  CategoryName: String,  
+  name: String,
+  img:{
+    data: Buffer,
+    contentType: String
+    },
+  options:Array,
+  description:String,
+  
+});
+
+const catschema= new Schema({
+  CategoryName:String
+});
+
+const FoodItem = mongoose.model('Food_item',mySchema,'Food_items');
+const Foodcategory = mongoose.model('Food_Category',catschema,'Food_category');
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const data = await FoodItem.find({});
+    const Catdata= await Foodcategory.find({});
+
+    console.log('Fetched Data:');
+
+    global.Food_items = data;
+    global.Food_category=Catdata;
+
+    console.log("connected to mongo")
+    const foodCollection = await mongoose.connection.db.collection("Food_items");
+    foodCollection.find({}).toArray(async function (err, data) {
+        const categoryCollection = await mongoose.connection.db.collection("Food_category");
+        categoryCollection.find({}).toArray(async function (err, Catdata) {
+            callback(err, data, Catdata);
+        })});
+
+
+  } catch (error) {
+    console.error('Error:', error);
+  } 
+};
+
+module.exports = mongoDB;
+
+
+
+
+
